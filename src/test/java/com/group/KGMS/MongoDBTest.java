dfb3d116c81e923b83c49a240371520a46f4f022
FxStu <112561859+FxStu@users.noreply.github.com>
2023-03-19 15:59:10 +0800

    后端爬虫接口

--- src/test/java/com/group/KGMS/MongoDBTest.java
+++ src/test/java/com/group/KGMS/MongoDBTest.java
@@ -1,16 +1,24 @@
 package com.group.KGMS;
 
+import com.group.KGMS.entity.Image;
 import com.group.KGMS.entity.Record;
 import com.group.KGMS.entity.UnstructuredText;
+import com.group.KGMS.entity.UnstructuredTextOriginal;
 import com.group.KGMS.repository.UnstructuredTextRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 public class MongoDBTest {
@@ -28,14 +36,15 @@ public class MongoDBTest {
     }
 
     @Test
-    public void insertrecord(){
-        Record record = new Record();
-        Date date=new Date();
-        SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String date1 = sdf.format(date);
-        record.setCid(1);
-        record.setStart_time(date1);
-        record.setStatus(1);
-        System.out.println(mongoTemplate.insert(record).get_id());
+    public void findDistinct(){
+        Pageable pageable=PageRequest.of(0,5);
+        //创建查询对象
+        Query query = new Query();
+        query.addCriteria(
+                new Criteria().and("cid").is(2)
+        );
+        query.with(pageable);
+        System.out.println(mongoTemplate.find(query, UnstructuredTextOriginal.class));
+//        return list;
     }
 }
